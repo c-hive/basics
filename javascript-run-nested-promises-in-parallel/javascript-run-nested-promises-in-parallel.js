91847ffeb110e772148d48f125279d7368893d54
Zsolt Gömöri <gomori.zsolt1995@gmail.com>
2020-03-11 15:06:45 +0100

    Replace for(;;;) loop with Array().fill()

--- javascript-run-nested-promises-in-parallel/javascript-run-nested-promises-in-parallel.js
+++ javascript-run-nested-promises-in-parallel/javascript-run-nested-promises-in-parallel.js
@@ -2,19 +2,15 @@ const randomDelay = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const uniqueIdentifier = () => '_' + Math.random().toString(36).substr(2, 9);
 const delayedPromise = (delayMin, delayMax) => new Promise(resolve => setTimeout(() => resolve(uniqueIdentifier()), randomDelay(delayMin, delayMax))); 
 
-const promises = [];
+const promises = new Array(5).fill().map((_, i) => delayedPromise(2000, 7000).then(outerPromiseId => {
+    console.log(`Resolved ${outerPromiseId} outer promise. Index: ${i}`);
 
-for (let i = 0; i < 5; i++) {
-    promises.push(delayedPromise(2000, 7000).then(outerPromiseId => {
-        console.log(`Resolved ${outerPromiseId} outer promise. Index: ${i}`);
+    return new Array(3).fill().map(() => delayedPromise(0, 1000).then(id => {
+        console.log(`Resolved ${id} inner promise for ${outerPromiseId} outer promise.`)
 
-        return new Array(3).fill().map(() => delayedPromise(0, 1000).then(id => {
-            console.log(`Resolved ${id} inner promise for ${outerPromiseId} outer promise.`)
-
-            return id;
-        }));
+        return id;
     }));
-}
+}))
 
 function run() {
     return Promise.all(promises).then(result =>
